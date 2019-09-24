baa28d3aafcc57ee0efb0f2cdbb45e7f4f653c8e
Iluminada Longtown <ilongtow@oa-j4.21-school.ru>
2019-09-24 14:35:28 +0300

    save

---
+++ libft/ft_strchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilongtow <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/24 12:56:30 by ilongtow          #+#    #+#             */
+/*   Updated: 2019/09/24 14:16:40 by ilongtow         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (unsigned char)c)
+			return ((char*)s);
+		else
+			s++;
+	}
+	if (*s == (unsigned char)c)
+		return ((char*)s);
+	else
+		return (NULL);
+}
---
+++ libft/libft.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilongtow <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/20 20:27:29 by ilongtow          #+#    #+#             */
+/*   Updated: 2019/09/24 13:32:23 by ilongtow         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <string.h>
+
+void		ft_putchar(char c);
+void		ft_putchar_fd(char c, int fd);
+void		ft_bzero(void *s, size_t n);
+void		*ft_memcpy(void *dst, const void *src, size_t n);
+void		*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void		*ft_memset(void *b, int c, size_t len);
+void		*ft_memchr(const void *s, int c, size_t n);
+void		*ft_memmove(void *dst, const void *src, size_t len);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
+char		*ft_strdup(const char *s1);
+char		*ft_strcpy(char * dst, const char * src);
+char		*ft_strncpy(char * dst, const char * src, size_t len);
+char		*ft_strcat(char *s1, const char *s2);
+char		*ft_strncat(char *s1, const char *s2, size_t n);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strchr(const char *s, int c);
+size_t		ft_strlcat(char *dest, const char *src, size_t size);
+size_t		ft_strlen(const char *str);
+
+#endif
