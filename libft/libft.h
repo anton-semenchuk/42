1bee85a5be946964280a6c4e90928c3eed5859c6
Iluminada Longtown <ilongtow@oa-f6.21-school.ru>
2019-09-26 21:40:49 +0300

    all functions

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ilongtow <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/20 20:27:29 by ilongtow          #+#    #+#             */
-/*   Updated: 2019/09/26 14:02:09 by ilongtow         ###   ########.fr       */
+/*   Updated: 2019/09/26 21:23:59 by ilongtow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,12 @@ void		ft_strdel(char **as);
 void		ft_strclr(char *s);
 void		ft_striter(char *s, void (*f)(char *));
 void		ft_striteri(char *s, void (*f)(unsigned int, char *));
+void		ft_putstr(char const *s);
+void		ft_putstr_fd(char const *s, int fd);
+void		ft_putendl(char const *s);
+void		ft_putendl_fd(char const *s, int fd);
+void		ft_putnbr(int n);
+void		ft_putnbr_fd(int n, int fd);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 int			ft_strcmp(const char *s1, const char *s2);
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
@@ -56,6 +62,7 @@ char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char		*ft_strsub(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_strtrim(char const *s);
+char		*ft_itoa(int n);
 char		**ft_strsplit(char const *s, char c);
 size_t		ft_strlcat(char *dest, const char *src, size_t size);
 size_t		ft_strlen(const char *str);
